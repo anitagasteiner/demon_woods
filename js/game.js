17cdef02bbdbf973d92fd69534975ea907ef72a7
Anita Gasteiner <anita.gasteiner@posteo.de>
2024-08-25 16:42:28 +0200

    track if key is not pressed any more

--- js/game.js
+++ js/game.js
@@ -27,3 +27,22 @@ window.addEventListener("keydown", (e) => {
         keyboard.RIGHT = true;
     }
 });
+
+window.addEventListener("keyup", (e) => {
+    if (e.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+    if (e.keyCode == 40) {
+        keyboard.DOWN = false;
+    }
+    if (e.keyCode == 38) {
+        keyboard.UP = false;
+    }
+    if (e.keyCode == 37) {
+        keyboard.LEFT = false;
+    }
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = false;
+    }
+
+});
