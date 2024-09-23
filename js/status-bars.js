cfd556903207420c3491e4c0fc993e3007aecbd4
Anita Gasteiner <anita.gasteiner@posteo.de>
2024-09-23 11:56:44 +0200

    change order of status bars

--- js/status-bars.js
+++ js/status-bars.js
@@ -1,11 +1,3 @@
-const PATHS_STATUS_APPLES = [
-    '../img/statusbars/apples/0.png',
-    '../img/statusbars/apples/20.png',
-    '../img/statusbars/apples/40.png',
-    '../img/statusbars/apples/60.png',
-    '../img/statusbars/apples/80.png',
-    '../img/statusbars/apples/100.png'
-];
 const PATHS_STATUS_HEALTH = [
     '../img/statusbars/health/0.png',
     '../img/statusbars/health/20.png',
@@ -13,6 +5,14 @@ const PATHS_STATUS_HEALTH = [
     '../img/statusbars/health/60.png',
     '../img/statusbars/health/80.png',
     '../img/statusbars/health/100.png'
+];
+const PATHS_STATUS_APPLES = [
+    '../img/statusbars/apples/0.png',
+    '../img/statusbars/apples/20.png',
+    '../img/statusbars/apples/40.png',
+    '../img/statusbars/apples/60.png',
+    '../img/statusbars/apples/80.png',
+    '../img/statusbars/apples/100.png'
 ];
 const PATHS_STATUS_CRYSTALS = [
     '../img/statusbars/crystals/0.png',
@@ -23,7 +23,7 @@ const PATHS_STATUS_CRYSTALS = [
     '../img/statusbars/crystals/100.png'
 ];
 const newStatusBars = [
-    new StatusBar(PATHS_STATUS_APPLES, 5, 0),
-    new StatusBar(PATHS_STATUS_HEALTH, 45, 100),
-    new StatusBar(PATHS_STATUS_CRYSTALS, 85, 0)
+    new StatusBar(PATHS_STATUS_HEALTH, 10, 100),
+    new StatusBar(PATHS_STATUS_APPLES, 60, 0),    
+    new StatusBar(PATHS_STATUS_CRYSTALS, 105, 0)
 ];
