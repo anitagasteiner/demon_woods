e4b42cc08e81bf8f2daa96f4b368d966b74d1d4f
Anita Gasteiner <anita.gasteiner@posteo.de>
2024-10-17 13:34:58 +0200

    smaller changes

--- js/game.js
+++ js/game.js
@@ -249,3 +249,15 @@ function closeFullscreen() {
       document.msExitFullscreen();
     }
   }
+
+  function handleMobileBar() {
+    /* Storing user's device details in a variable*/
+    let details = navigator.userAgent;
+    /* Creating a regular expression containing some mobile devices keywords to search it in details string*/
+    let regexp = /android|iphone|kindle|ipad/i;
+    /* Using test() method to search regexp in details it returns boolean value*/
+    let isMobileDevice = regexp.test(details);
+    if (isMobileDevice) { 
+        document.getElementById('mobileBar').classList.remove('hide');
+    }      
+  }
